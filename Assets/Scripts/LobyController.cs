3fbf6706d6cd16b96a19cdc699dd705648fb2214
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2025-01-20 02:57:08 -0600

    feat(niveles-nuevos)se añaden asssets para los niveles nuevos

--- Assets/Scripts/LobyController.cs
+++ Assets/Scripts/LobyController.cs
@@ -4,16 +4,12 @@ using UnityEngine.UI;
 
 public class LobyController : MonoBehaviour
 {
-    public Button btnPerlas;
-    public Button btnBebidas;
-    public Button btnClips;
+    public int LevelIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        btnPerlas.onClick.AddListener(OnPerlasClick);
-        btnBebidas.onClick.AddListener(OnBebidasClick);
-        btnClips.onClick.AddListener(OnClipsClick);
+
     }
 
     // Update is called once per frame
@@ -22,23 +18,8 @@ public class LobyController : MonoBehaviour
 
     }
 
-    private void LoadLevelIndex(int level)
+    public void LoadLevelIndex(int level)
     {
         SceneManager.LoadScene(level);
-    }
-
-    void OnPerlasClick()
-    {
-        LoadLevelIndex(0);
-    }
-
-    void OnBebidasClick()
-    {
-        LoadLevelIndex(1);
-    }
-
-    void OnClipsClick()
-    {
-        LoadLevelIndex(2);
     }
 }
